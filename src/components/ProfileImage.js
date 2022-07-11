8dd57d245419383c147adfa769e9eb5f6d09ecd3
lluisllc <lluis.llacera@gmail.com>
2022-07-11 16:53:49 +0200

    Iteración 8 + imagen OK

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,18 +1,9 @@
-// function Tweet(props) {
-//     console.log(props)
-//     return (
-//       <div className="tweet">
-//         <img
-//           src={props.tweet.user.image}
-//           className="profile"
-//           alt="profile"
-//         />
 import React from "react";
 
 function ProfileImage(props) {
     console.log(props)
   return (
-    <div className="tweet">
+    <div className="profile">
       <img src={props.image} className="profile" alt="profile" />
     </div>
   );
